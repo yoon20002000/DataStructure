7039405dc7b78cf53a804c986d41d6741ad83a67
yoon20002000 <yoon20002000@naver.com>
2025-04-16 11:38:08 +0900

    BT Order Prac02

---
+++ Pracs/Prac02/DataStructures/BT/BTPrac02.cpp
@@ -0,0 +1,67 @@
+﻿#include "BTPrac02.h"
+
+#include <iostream>
+
+BTPrac02Node::BTPrac02Node(char c) : data(c), left(nullptr), right(nullptr)
+{
+}
+
+BTPrac02Node::~BTPrac02Node()
+{
+}
+
+BTPrac02Node::BTPrac02Node(BTPrac02Node&& other) noexcept
+{
+    left = other.left;
+    right = other.right;
+    data = other.data;
+
+    other.left = nullptr;
+    other.right = nullptr;
+    other.data = '\0';
+}
+
+BTPrac02Node& BTPrac02Node::operator=(BTPrac02Node&& other) noexcept
+{
+    left = other.left;
+    right = other.right;
+    data = other.data;
+
+    other.left = nullptr;
+    other.right = nullptr;
+    other.data = '';
+}
+
+void BTPrac02::printPreorder(BTPrac02Node* root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    std::cout << root->data;
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+
+void BTPrac02::printInorder(BTPrac02Node* root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    printInorder(root->left);
+    std::cout << root->data;
+    printInorder(root->right);
+}
+
+void BTPrac02::printPostorder(BTPrac02Node* root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    printPostorder(root->left);
+    printPostorder(root->right);
+    std::cout << root->data;
+}
---
+++ Pracs/Prac02/DataStructures/BT/BTPrac02.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class BTPrac02Node
+{
+public:
+    BTPrac02Node(char c);
+    ~BTPrac02Node();
+    BTPrac02Node(const BTPrac02Node& other) = delete;
+    BTPrac02Node& operator=(const BTPrac02Node& other) = delete;
+    BTPrac02Node(BTPrac02Node&& other) noexcept;
+    BTPrac02Node& operator=(BTPrac02Node&& other) noexcept;
+
+    char data;
+    BTPrac02Node* left;
+    BTPrac02Node* right;
+};
+
+class BTPrac02
+{
+public:
+    static void printPreorder(BTPrac02Node* root);
+    static void printInorder(BTPrac02Node* root);
+    static void printPostorder(BTPrac02Node* root);
+};
