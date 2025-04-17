793b85dfec7931b2afb751849bded99f53e3d791
yoon20002000 <yoon20002000@naver.com>
2025-04-17 12:57:35 +0900

    SelectionSort 03

---
+++ Pracs/Prac03/Sort/SelectionSort/SelectionSort03.cpp
@@ -0,0 +1,21 @@
+﻿#include "SelectionSort03.h"
+
+#include <memory>
+void SelectionSort03::sort(int* arr, int size)
+{
+    for (int i = 0 ; i < size ; ++i)
+    {
+        int minIndex = i;
+        for (int j = i +1; j < size; ++j)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            std::swap(arr[i], arr[minIndex]);    
+        }
+    }
+}
