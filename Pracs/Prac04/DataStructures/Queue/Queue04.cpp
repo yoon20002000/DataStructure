20eb1709328ebc09f70452dccb6947cf33935ae6
yoon20002000 <yoon20002000@naver.com>
2025-04-18 09:31:45 +0900

    Queue04

---
+++ Pracs/Prac04/DataStructures/Queue/Queue04.cpp
@@ -0,0 +1,157 @@
+﻿#include "Queue04.h"
+
+Queue04Node::Queue04Node() : data(0), next(nullptr)
+{
+}
+
+Queue04Node::Queue04Node(int data, Queue04Node* next) : data(data), next(next)
+{
+}
+
+Queue04Node::~Queue04Node()
+{
+}
+
+Queue04Node::Queue04Node(Queue04Node&& other) noexcept
+{
+    data = other.data;
+    next = other.next;
+
+    other.data= 0;
+    other.next = nullptr;
+}
+
+Queue04Node& Queue04Node::operator=(Queue04Node&& other) noexcept
+{
+    if (&other == this)
+    {
+        return *this;
+    }
+    
+    data = other.data;
+    next = other.next;
+
+    other.data= 0;
+    other.next = nullptr;
+
+    return *this;
+}
+
+Queue04::Queue04() : front(nullptr), rear(nullptr)
+{
+}
+
+Queue04::~Queue04()
+{
+    clear();   
+}
+
+Queue04::Queue04(const Queue04& other) : front(nullptr), rear(nullptr)
+{
+    copyFrom(other);
+}
+
+Queue04& Queue04::operator=(const Queue04& other)
+{
+    if (&other == this)
+    {
+        return *this;
+    }
+    clear();
+    copyFrom(other);
+    return *this;
+}
+
+Queue04::Queue04(Queue04&& other) noexcept
+{
+    front  = other.front;
+    rear = other.rear;
+
+    other.front = nullptr;
+    other.rear = nullptr;
+}
+
+Queue04& Queue04::operator=(Queue04&& other) noexcept
+{
+    if (&other == this)
+    {
+        return *this;
+    }
+
+    clear();
+    front = other.front;
+    rear = other.rear;
+
+    other.front = nullptr;
+    other.rear = nullptr;
+    
+    return *this;
+}
+
+int Queue04::peek() const
+{
+    if (isEmpty())
+    {
+        return -1;
+    }
+
+    return front->data;
+}
+
+void Queue04::enqueue(int data)
+{
+    Queue04Node* newNode = new Queue04Node(data);
+    
+    if (isEmpty())
+    {
+        front = rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void Queue04::dequeue()
+{
+    if (isEmpty())
+    {
+        return;
+    }
+    
+    Queue04Node* nextFront = front->next;
+    delete front;
+    front = nextFront;
+    
+    if (isEmpty())
+    {
+        rear = nullptr;
+    }
+}
+
+bool Queue04::isEmpty() const
+{
+    return front == nullptr;
+}
+
+void Queue04::copyFrom(const Queue04& other)
+{
+    Queue04Node* curNode = other.front;
+    while (curNode!=nullptr)
+    {
+        enqueue(curNode->data);
+        
+        curNode = curNode->next;
+    }
+}
+
+void Queue04::clear()
+{
+    while (front != nullptr)
+    {
+        Queue04Node* nextFront = front->next;
+        delete front;
+        front = nextFront;
+    }
+}
---
+++ Pracs/Prac04/DataStructures/Queue/Queue04.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+class Queue04Node
+{
+public:
+    Queue04Node();
+    Queue04Node(int data, Queue04Node* next = nullptr);
+    ~Queue04Node();
+    Queue04Node(const Queue04Node& other) = delete;
+    Queue04Node& operator=(const Queue04Node& other) = delete;
+    Queue04Node(Queue04Node&& other) noexcept;
+    Queue04Node& operator=(Queue04Node&& other)noexcept;
+    
+public:
+    int data;
+    Queue04Node* next;
+};
+
+class Queue04
+{
+public:
+    Queue04();
+    ~Queue04();
+    Queue04(const Queue04& other);
+    Queue04& operator=(const Queue04& other);
+    Queue04(Queue04&& other) noexcept;
+    Queue04& operator=(Queue04&& other)noexcept;
+
+    int peek() const;
+    void enqueue(int data);
+    void dequeue();
+    bool isEmpty() const;
+private:
+    void copyFrom(const Queue04& other);
+    void clear();
+public:
+    Queue04Node* front;
+    Queue04Node* rear;
+};
