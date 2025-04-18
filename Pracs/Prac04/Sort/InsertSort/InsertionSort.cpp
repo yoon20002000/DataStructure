acac20988235be882b1469f42c7b7c90e72cd9a7
yoon20002000 <yoon20002000@naver.com>
2025-04-18 12:37:07 +0900

    InserstionSort 04

---
+++ Pracs/Prac04/Sort/InsertSort/InsertionSort.cpp
@@ -0,0 +1,21 @@
+﻿#include "InsertionSort.h"
+
+void InsertionSort::sort(int* arr, int size)
+{
+    if (size < 2)
+    {
+        return;
+    }
+
+    for (int i = 1 ; i < size; ++i)
+    {
+        int key = arr[i];
+        int j = i ;
+        while (j > 0 && arr[j-1] > key)
+        {
+            arr[j] = arr[j-1];
+            j--;
+        }
+        arr[j] = key;
+    }
+}
