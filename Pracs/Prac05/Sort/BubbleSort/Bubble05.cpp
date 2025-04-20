d19ab8095845e0335c69ab2fcb81d6e170aac040
yoon20002000 <yoon20002000@naver.com>
2025-04-20 14:38:10 +0900

    Bubble 05

---
+++ Pracs/Prac05/Sort/BubbleSort/Bubble05.cpp
@@ -0,0 +1,28 @@
+﻿#include "Bubble05.h"
+
+#include <memory>
+
+void Bubble05::sort(int* arr, int size)
+{
+    if (size < 2)
+    {
+        return;
+    }
+    
+    for (int i = 0 ; i < size-1; i++)
+    {
+        bool isSwapped = false;
+        for (int j = 0; j < size - 1 - i; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+                isSwapped = true;
+            }
+        }
+        if (!isSwapped)
+        {
+            break;
+        }
+    }
+}
---
+++ Pracs/Prac05/Sort/BubbleSort/Bubble05.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class Bubble05
+{
+public:
+    void sort(int* arr, int size);
+};
