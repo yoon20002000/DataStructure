760b3f2f5dce7fc5b0feabca20f9fbeed74a4c04
yoon20002000 <yoon20002000@naver.com>
2025-04-20 14:47:12 +0900

    SelectionSort 05

---
+++ Pracs/Prac05/Sort/SelectionSort/SelectionSort05.cpp
@@ -0,0 +1,27 @@
+﻿#include "SelectionSort05.h"
+
+#include<memory>
+
+void SelectionSort05::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+    
+    for (int i = 0 ; i < size; i++)
+    {
+        int minIndex = i;
+        for (int j = i+1; j < size; j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            std::swap(arr[minIndex], arr[i]);    
+        }
+    }
+}
