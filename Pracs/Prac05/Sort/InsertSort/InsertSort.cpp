406079a4934e8c79f7887a8787db9838a1ebb19e
yoon20002000 <yoon20002000@naver.com>
2025-04-20 14:43:02 +0900

    InsertionSort05

---
+++ Pracs/Prac05/Sort/InsertSort/InsertSort.cpp
@@ -0,0 +1,21 @@
+﻿#include "InsertSort.h"
+
+void InsertSort::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+    
+    for (int i = 1 ; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i ;
+        while (j > 0 && key< arr[j - 1])
+        {
+            arr[j]=arr[j-1];
+            --j;
+        }
+        arr[j]=key;
+    }
+}
---
+++ Pracs/Prac05/Sort/InsertSort/InsertSort.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class InsertSort
+{
+public:
+    static void sort(int* arr, int size);
+};
