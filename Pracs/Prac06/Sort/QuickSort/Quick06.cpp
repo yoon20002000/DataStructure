e812e3020e67c72f8640b492d8a8a1db1667d2ed
yoon20002000 <yoon20002000@naver.com>
2025-04-21 14:02:43 +0900

    Quick 06

---
+++ Pracs/Prac06/Sort/QuickSort/Quick06.cpp
@@ -0,0 +1,63 @@
+﻿#include "Quick06.h"
+
+#include <memory>
+
+void Quick06::sort(int* arr, int left, int right)
+{
+    if (left < right)
+    {
+        int pivot = partitionH(arr, left, right);
+        sort(arr, left, pivot-1);
+        sort(arr, pivot+1, right);
+    }
+}
+
+int Quick06::partitionH(int* arr, int left, int right)
+{
+    int pivot = left + (right - left) / 2;
+    int pivotItem = arr[pivot];
+
+    int l = left - 1;
+    int r = right + 1;
+    
+    while (true)
+    {
+        do
+        {
+            l++;
+        }while (arr[l] < pivotItem);
+        do
+        {
+            r--;
+        }
+        while (arr[r] > pivotItem);
+        
+        if (l<r)
+        {
+            std::swap(arr[l], arr[r]);
+        }
+        else
+        {
+            return r;
+        }
+    }
+}
+
+int Quick06::partitionL(int* arr, int left, int right)
+{
+    int pivot = left;
+    int pivotItem = arr[pivot];
+    if (left<right)
+    {
+        for (int i = left + 1; i <=right; ++i)
+        {
+            if (arr[i]<pivotItem)
+            {
+                pivot++;
+                std::swap(arr[pivot], arr[i]);
+            }
+        }
+        std::swap(arr[pivot], arr[left]);
+    }
+    return pivot;
+}
---
+++ Pracs/Prac06/Sort/QuickSort/Quick06.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+class Quick06
+{
+public:
+    static void sort(int* arr, int left, int right);
+    static int partitionH(int* arr, int left, int right);
+    static int partitionL(int*arr, int left, int right);
+};
