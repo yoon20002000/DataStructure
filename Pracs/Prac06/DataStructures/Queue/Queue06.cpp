b74167222ec2d34b2e8d1f7af074c5efc207ee48
yoon20002000 <yoon20002000@naver.com>
2025-04-21 11:55:18 +0900

    Queue06

---
+++ Pracs/Prac06/DataStructures/Queue/Queue06.cpp
@@ -0,0 +1,91 @@
+﻿#include "Queue06.h"
+
+Queue06Node::Queue06Node(int data, Queue06Node* next) : data(data), next(next)
+{
+}
+
+Queue06Node::~Queue06Node()
+{
+}
+
+int Queue06Node::getData() const
+{
+    return data;
+}
+
+void Queue06Node::setData(int inData)
+{
+    data = inData;
+}
+
+Queue06Node* Queue06Node::getNext() const
+{
+    return next;
+}
+
+void Queue06Node::setNext(Queue06Node* inNext)
+{
+    next = inNext;
+}
+Queue06::Queue06(): size(0), front(nullptr), back(nullptr)
+{
+}
+
+Queue06::~Queue06()
+{
+    deleteQueue();
+}
+
+void Queue06::enqueue(int data)
+{
+    Queue06Node* newNode = new Queue06Node(data, nullptr);
+
+    if (front == nullptr)
+    {
+        front = back = newNode;
+    }
+    else
+    {
+        back->setNext(newNode);
+        back = newNode;
+    }
+    ++size;
+}
+
+int Queue06::dequeue()
+{
+    if (isEmpty())
+    {
+        return -1;
+    }
+
+    int returnValue = front->getData();
+    Queue06Node* nextFront = front->getNext();
+    delete front;
+    front = nextFront;
+
+    if (front == nullptr)
+    {
+        back = nullptr;
+    }
+    --size;
+    return returnValue;
+}
+
+bool Queue06::isEmpty() const
+{
+    return front == nullptr;
+}
+
+int Queue06::getSize() const
+{
+    return size;
+}
+
+void Queue06::deleteQueue()
+{
+    while (front != nullptr)
+    {
+        dequeue();
+    }
+}
---
+++ Pracs/Prac06/DataStructures/Queue/Queue06.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+class Queue06Node
+{
+public:
+    Queue06Node() = delete;
+    Queue06Node(int data, Queue06Node* next = nullptr);
+    ~Queue06Node();
+    Queue06Node(const Queue06Node&) = delete;
+    Queue06Node& operator=(const Queue06Node&) = delete;
+    Queue06Node(Queue06Node&&) = delete;
+    Queue06Node& operator=(Queue06Node&&) = delete;
+
+    int getData() const;
+    void setData(int inData);
+    Queue06Node* getNext() const;
+    void setNext(Queue06Node* inNext);
+    
+private:
+    int data;
+    Queue06Node* next;
+};
+
+class Queue06
+{
+public:
+    Queue06();
+    ~Queue06();
+    Queue06(const Queue06& other) = delete;
+    Queue06& operator=(const Queue06& other) = delete;
+    Queue06(Queue06&& other) = delete;
+    Queue06& operator=(Queue06&& other) = delete;
+
+    void enqueue(int data);
+    int dequeue();
+
+    bool isEmpty() const;
+    int getSize() const;
+private:
+    void deleteQueue();
+    
+private:
+    int size;
+    Queue06Node* front;
+    Queue06Node* back;
+};
