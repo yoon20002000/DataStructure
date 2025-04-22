a4fa6b8d5421677a4a736c3690d9466c819f597a
yoon20002000 <yoon20002000@naver.com>
2025-04-22 18:02:49 +0900

    Queue07

---
+++ Pracs/Prac07/DataStructures/Queue/Queue07.cpp
@@ -0,0 +1,59 @@
+﻿#include "Queue07.h"
+
+#include <iostream>
+
+Queue07::Queue07(int size) : capacity(size), front(0), rear(0)
+{
+    data = new int[capacity];
+}
+
+Queue07::~Queue07()
+{
+    delete [] data;
+}
+
+void Queue07::enqueue(int value)
+{
+    if (isFull())
+    {
+        std::cout << "Queue is full\n";
+        return;
+    }
+    data[rear] = value;
+    rear = (rear + 1) % capacity;
+}
+
+int Queue07::dequeue()
+{
+    if (isEmpty())
+    {
+        return -1;
+    }
+    int returnValue = data[front];
+    front = (front+1) % capacity;
+    return returnValue;
+}
+
+int Queue07::peak() const
+{
+    if (isEmpty())
+    {
+        return -1;
+    }
+    return data[front];
+}
+
+int Queue07::size() const
+{
+    return (rear - front + capacity) % capacity;
+}
+
+bool Queue07::isEmpty() const
+{
+    return rear == front;
+}
+
+bool Queue07::isFull() const
+{
+    return (rear + 1) % capacity == front;
+}
---
+++ Pracs/Prac07/DataStructures/Queue/Queue07.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+class Queue07
+{
+public:
+    Queue07(int size);
+    ~Queue07();
+    Queue07(const Queue07& other);
+    Queue07& operator=(const Queue07& other);
+    Queue07(Queue07&& other) noexcept;
+    Queue07& operator=(Queue07&& other) noexcept;
+
+    void enqueue(int value);
+    int dequeue();
+    int peak() const;
+    int size() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    
+private:
+    int* data;
+    int capacity;
+    int front;
+    int rear;
+};
