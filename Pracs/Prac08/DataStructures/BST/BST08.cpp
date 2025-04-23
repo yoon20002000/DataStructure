91cdc2791b5cd1896677645beb252e2e4f06f6f2
yoon20002000 <yoon20002000@naver.com>
2025-04-24 00:14:56 +0900

    BST08

---
+++ Pracs/Prac08/DataStructures/BST/BST08.cpp
@@ -0,0 +1,192 @@
+﻿#include "BST08.h"
+
+#include <iostream>
+
+BST08Node::BST08Node(int inData) : data(inData), left(nullptr), right(nullptr)
+{
+}
+
+BST08::BST08() : root(nullptr)
+{
+    
+}
+
+BST08::~BST08()
+{
+    root = deleteBST(root);
+}
+
+BST08::BST08(const BST08& other)
+{
+    root = copyBST(other.root);
+}
+
+BST08& BST08::operator=(const BST08& other)
+{
+    if (&other != this)
+    {
+        root = deleteBST(root);
+
+        root = copyBST(other.root);
+    }
+    return *this;
+}
+
+BST08::BST08(BST08&& other) noexcept
+{
+    root = other.root;
+
+    other.root = nullptr;
+}
+
+BST08& BST08::operator=(BST08&& other) noexcept
+{
+    if (&other != this)
+    {
+        root = deleteBST(root);
+        
+        root = other.root;
+
+        other.root = nullptr;
+    }
+    return *this;
+}
+
+void BST08::insertData(int data)
+{
+    root = insertData(root, data);
+}
+
+void BST08::deleteData(int data)
+{
+    root = deleteData(root, data);
+}
+
+BST08Node* BST08::searchData(int data)
+{
+    return searchData(root, data);
+}
+
+BST08Node* BST08::insertData(BST08Node* node, int data)
+{
+    if (node == nullptr)
+    {
+        return new BST08Node(data);
+    }
+
+    if (data == node->data)
+    {
+        std::cout << "Data " << data << " already exists." << '\n';
+        return node;
+    }
+
+    if (data < node->data)
+    {
+        node->left = insertData(node->left, data);    
+    }
+    else
+    {
+        node->right = insertData(node->right, data);
+    }
+    
+    return node;
+}
+
+BST08Node* BST08::deleteData(BST08Node* node, int data)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (node->data == data)
+    {
+        if (node->left == nullptr && node->right == nullptr)
+        {
+            delete node;
+            return nullptr;
+        }
+        else if (node->left == nullptr || node->right == nullptr)
+        {
+            BST08Node* childNode = node->left ? node->left : node->right;
+            delete node;
+            return childNode;
+        }
+        else
+        {
+            BST08Node* maxNode = findMax(node->left);
+            node->data = maxNode->data;
+            node->left = deleteData(node->left, node->data);
+        }
+    }
+    else
+    {
+        if (node->data < data)
+        {
+            node->right = deleteData(node->right, data);
+        }
+        else
+        {
+            node->left = deleteData(node->left, data);
+        }
+    }
+
+    return node;
+}
+
+BST08Node* BST08::searchData(BST08Node* node, int data)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (node->data == data)
+    {
+        return node;
+    }
+    else if (node->data < data)
+    {
+        return searchData(node->right, data);
+    }
+    else
+    {
+        return searchData(node->left, data);
+    }
+}
+
+BST08Node* BST08::copyBST(BST08Node* node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+
+    BST08Node* newNode = new BST08Node(node->data);
+    newNode->left = copyBST(node->left);
+    newNode->right = copyBST(node->right);
+
+    return newNode;
+}
+
+BST08Node* BST08::deleteBST(BST08Node* node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+
+    node->left = deleteBST(node->left);
+    node->right = deleteBST(node->right);
+
+    delete node;
+    return nullptr;
+}
+BST08Node* BST08::findMax(BST08Node* node) const
+{
+    while (node != nullptr && node->right != nullptr)
+    {
+        node = node->right;
+    }
+    return node;
+}
---
+++ Pracs/Prac08/DataStructures/BST/BST08.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+class BST08Node
+{
+public:
+    BST08Node(int inData);
+    ~BST08Node() = default;
+    BST08Node(const BST08Node& other) = delete;
+    BST08Node& operator=(const BST08Node& other) = delete;
+    BST08Node(BST08Node&& other) = delete;
+    BST08Node& operator=(BST08Node&& other) = delete;
+    
+private:
+    int data;
+    BST08Node* left;
+    BST08Node* right;
+
+    friend class BST08;
+};
+
+class BST08
+{
+public:
+    BST08();
+    ~BST08();
+    BST08(const BST08& other);
+    BST08& operator=(const BST08& other);
+    BST08(BST08&& other) noexcept;
+    BST08& operator=(BST08&& other) noexcept;
+
+    void insertData(int data);
+    void deleteData(int data);
+    BST08Node* searchData(int data);
+    
+private:
+    BST08Node* insertData(BST08Node* node, int data);
+    BST08Node* deleteData(BST08Node* node, int data);
+    BST08Node* searchData(BST08Node* node, int data);
+    
+    BST08Node* copyBST(BST08Node* node);
+    BST08Node* deleteBST(BST08Node* node);
+    BST08Node* findMax(BST08Node* node) const;
+private:
+    BST08Node* root;
+};
