51368b5222062528f0b20fa468c0d470506ea958
yoon20002000 <yoon20002000@naver.com>
2025-04-24 02:30:41 +0900

    insertion 08

---
+++ Pracs/Prac08/Sort/InsertSort/Insertion08.cpp
@@ -0,0 +1,21 @@
+﻿#include "Insertion08.h"
+
+void Insertion08::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+
+    for (int i = 1; i < size; ++i)
+    {
+        int item = arr[i];
+        int j = i;
+        while (j > 0 && arr[j - 1] > item)
+        {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = item;
+    }
+}
---
+++ Pracs/Prac08/Sort/InsertSort/Insertion08.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class Insertion08
+{
+public:
+    static void sort(int* arr, int size);
+};
