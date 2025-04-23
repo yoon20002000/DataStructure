c2376ff5138a21ad57c4197f7138f22fe11623f7
yoon20002000 <yoon20002000@naver.com>
2025-04-23 23:07:03 +0900

    stack08

---
+++ Pracs/Prac08/DataStructures/Stack/Stack08.cpp
@@ -0,0 +1,131 @@
+﻿#include "Stack08.h"
+
+#include <iostream>
+#include <ostream>
+
+Stack08::Stack08(int inCapacity) : data(nullptr), capacity(0), size(0)
+{
+    reserve(inCapacity);
+}
+
+Stack08::~Stack08()
+{
+    delete [] data;
+}
+
+Stack08::Stack08(const Stack08& other) : data(nullptr), capacity(0), size(other.size)
+{
+    reserve(other.capacity);
+    for (int i = 0 ; i < other.size; ++i)
+    {
+        data[i] = other.data[i];
+    }
+}
+
+Stack08& Stack08::operator=(const Stack08& other)
+{
+    if (&other != this)
+    {
+        delete [] data;
+        
+        reserve(other.capacity);
+        for (int i = 0 ; i < other.size; ++i)
+        {
+            data[i] = other.data[i];
+        }
+    }
+    return *this;
+}
+
+Stack08::Stack08(Stack08&& other) noexcept
+{
+    data = other.data;
+    capacity = other.capacity;
+    size = other.size;
+
+    other.data = nullptr;
+    other.capacity = 0;
+    other.size = 0;
+}
+
+Stack08& Stack08::operator=(Stack08&& other) noexcept
+{
+    if (&other != this)
+    {
+        delete [] data;
+        
+        data = other.data;
+        capacity = other.capacity;
+        size = other.size;
+
+        other.data = nullptr;
+        other.capacity = 0;
+        other.size = 0;
+    }
+    return *this;
+}
+
+void Stack08::push(int value)
+{
+    if (capacity == size)
+    {
+        int newCapacity = static_cast<int>(capacity * 1.5f);
+        if (newCapacity == capacity)
+        {
+            ++newCapacity;
+        }
+        
+        reserve(newCapacity);
+    }
+    data[size++] = value;
+}
+
+void Stack08::pop()
+{
+    if (isEmpty())
+    {
+        std::cout << "Stack is empty." << '\n';
+        return;
+    }
+    --size;
+}
+
+int Stack08::peek() const
+{
+    if (isEmpty())
+    {
+        std::cout << "Stack is empty." << '\n';
+        return -1;
+    }
+
+    return data[size-1];
+}
+
+bool Stack08::isEmpty() const
+{
+    return size == 0;
+}
+
+int Stack08::getSize() const
+{
+    return size;
+}
+
+void Stack08::reserve(int newCapacity)
+{
+    if (capacity >= newCapacity)
+    {
+        return;
+    }
+
+    capacity = newCapacity;
+    int* newData = new int[capacity];
+
+    for (int i = 0 ; i < size; ++i)
+    {
+        newData[i] = data[i];
+    }
+    
+    delete[] data;
+    data = newData;
+}
---
+++ Pracs/Prac08/DataStructures/Stack/Stack08.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class Stack08
+{
+public:
+    Stack08(int inCapacity = 1);
+    ~Stack08();
+    Stack08(const Stack08& other);
+    Stack08& operator=(const Stack08& other);
+    Stack08(Stack08&& other) noexcept;
+    Stack08& operator=(Stack08&& other) noexcept;
+
+    void push(int value);
+    void pop();
+    int peek() const;
+    bool isEmpty() const;
+    int getSize() const;
+    void reserve(int newCapacity);
+
+private:
+    int* data;
+    int capacity;
+    int size;
+};
