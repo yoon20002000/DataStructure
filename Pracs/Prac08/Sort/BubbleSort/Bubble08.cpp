8654ab7693d55e6395a4fb27b8b379849bb73693
yoon20002000 <yoon20002000@naver.com>
2025-04-24 02:23:35 +0900

    Bubble08

---
+++ Pracs/Prac08/Sort/BubbleSort/Bubble08.cpp
@@ -0,0 +1,28 @@
+﻿#include "Bubble08.h"
+
+#include <memory>
+
+void Bubble08::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+    
+    for (int i = 0 ; i < size - 1; ++i)
+    {
+        bool isSwapped = false;
+        for (int j = 0 ; j < size - 1 - i; ++j)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                std::swap(arr[j], arr[j+1]);
+                isSwapped = true;
+            }
+        }
+        if (isSwapped == false)
+        {
+            break;
+        }
+    }
+}
---
+++ Pracs/Prac08/Sort/BubbleSort/Bubble08.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class Bubble08
+{
+public:
+    static void sort(int* arr,int size);
+};
