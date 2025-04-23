7cf01421d895600cb7d6a8f2c6c65309c7446d45
yoon20002000 <yoon20002000@naver.com>
2025-04-24 02:27:25 +0900

    Selection08

---
+++ Pracs/Prac08/Sort/SelectionSort/Selection08.cpp
@@ -0,0 +1,26 @@
+﻿#include "Selection08.h"
+
+#include <memory>
+
+void Selection08::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return ;
+    }
+    for (int i = 0 ; i < size;++i)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j<size;++j)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            std::swap(arr[i], arr[minIndex]);    
+        }
+    }
+}
