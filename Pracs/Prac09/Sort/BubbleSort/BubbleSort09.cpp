a8c613c3d67429da37bb159cd1ec1184ff1d9dc5
yoon20002000 <yoon20002000@naver.com>
2025-04-24 11:28:20 +0900

    Bubble Sort09

---
+++ Pracs/Prac09/Sort/BubbleSort/BubbleSort09.cpp
@@ -0,0 +1,29 @@
+﻿#include "BubbleSort09.h"
+
+#include <memory>
+
+void BubbleSort09::sort(int* arr, int size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        bool isSwapped = false;
+        for (int j = 0 ; j < size - 1 -i;++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j+1]);
+                isSwapped = true;    
+            }
+        }
+
+        if (isSwapped == false)
+        {
+            break;
+        }
+    }
+}
---
+++ Pracs/Prac09/Sort/BubbleSort/BubbleSort09.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class BubbleSort09
+{
+public:
+    static void sort(int* arr, int size);
+};
