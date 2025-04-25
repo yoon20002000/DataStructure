dcae504047f84f328128d6cef0d1e5338d99629e
yoon20002000 <yoon20002000@naver.com>
2025-04-25 12:37:01 +0900

    Selection Sort 10

---
+++ Pracs/Prac10/Sort/SelectionSort/SelectionSort10.cpp
@@ -0,0 +1,27 @@
+﻿#include "SelectionSort10.h"
+
+#include <memory>
+
+void SelectionSort10::sort(int* arr, size_t size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < size; ++j)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (i != minIndex)
+        {
+            std::swap(arr[minIndex], arr[i]);
+        }
+    }
+}
