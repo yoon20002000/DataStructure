438718901532ef240cbdda65d4dd84676905c89e
yoon20002000 <yoon20002000@naver.com>
2025-04-25 12:30:06 +0900

    Bubble 10

---
+++ Pracs/Prac10/Sort/BubbleSort/BubbleSort10.cpp
@@ -0,0 +1,28 @@
+﻿#include "BubbleSort10.h"
+
+#include <memory>
+
+void BubbleSort10::sort(int* arr, size_t size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+
+    for (size_t i = 0 ; i < size - 1; ++i)
+    {
+        bool isSwapped = false;
+        for (size_t j = 0 ; j < size - 1 - i; ++j)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                std::swap(arr[j], arr[j+1]);
+                isSwapped = true;    
+            }
+        }
+        if (!isSwapped)
+        {
+            break;
+        }
+    }
+}
