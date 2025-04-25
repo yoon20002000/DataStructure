6a9b454817a62215ee91f584130377ac77c7f080
yoon20002000 <yoon20002000@naver.com>
2025-04-25 12:26:05 +0900

    HashTable10

---
+++ Pracs/Prac10/DataStructures/HashTable/HashTable10.cpp
@@ -0,0 +1,48 @@
+﻿#include "HashTable10.h"
+
+HashTable10::HashTable10() : table(TABLE_SIZE)
+{
+}
+
+void HashTable10::insert(int key, const std::string& value)
+{
+    int idx = hash(key);
+    for (std::pair<int, std::string>& pair : table[idx])
+    {
+        if (pair.first == key)
+        {
+            pair.second = value;
+            return;
+        }
+    }
+    table[idx].emplace_back(key, value);
+}
+
+bool HashTable10::remove(int key)
+{
+    int idx = hash(key);
+    
+    for (std::list<std::pair<int,std::string>>::iterator it = table[idx].begin(); it != table[idx].end(); ++it)
+    {
+        if (it->first == key)
+        {
+            table[idx].erase(it);
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+std::string* HashTable10::get(int key)
+{
+    int idx = hash(key);
+    for (std::pair<int, std::string>& pair : table[idx])
+    {
+        if (pair.first == key)
+        {
+            return &(pair.second);
+        }
+    }
+    return nullptr;
+}
---
+++ Pracs/Prac10/DataStructures/HashTable/HashTable10.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <list>
+#include <string>
+#include <vector>
+
+class HashTable10
+{
+private:
+    static const int TABLE_SIZE = 10;
+    std::vector<std::list<std::pair<int,std::string>>> table;
+    inline int hash(int key) const
+    {
+        return key % TABLE_SIZE;
+    }
+public:
+    HashTable10();
+    void insert(int key, const std::string& value);
+    bool remove(int key);
+    std::string* get(int key);
+};
