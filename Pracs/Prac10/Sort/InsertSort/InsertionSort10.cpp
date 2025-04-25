a3073cd6bbcd70f88726cb366d593600a7645fa3
yoon20002000 <yoon20002000@naver.com>
2025-04-25 12:44:11 +0900

    InsertionSort 10

---
+++ Pracs/Prac10/Sort/InsertSort/InsertionSort10.cpp
@@ -0,0 +1,21 @@
+﻿#include "InsertionSort10.h"
+
+void InsertionSort10::sort(int* arr, size_t size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+    for (size_t i = 1; i < size; ++i)
+    {
+        int pivotItem = arr[i];
+        size_t j = i;
+        while (j > 0 && arr[j-1] > pivotItem)
+        {
+            arr[j] = arr[j-1];
+            --j;
+        }
+
+        arr[j] = pivotItem;
+    }
+}
---
+++ Pracs/Prac10/Sort/InsertSort/InsertionSort10.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class InsertionSort10
+{
+public:
+    static void sort(int* arr, size_t size);
+};
