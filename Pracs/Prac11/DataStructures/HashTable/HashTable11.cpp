9ff0f6705774a5bb064098378586de3ea996bcff
yoon20002000 <yoon20002000@naver.com>
2025-04-27 21:18:51 +0900

    HashTable11

---
+++ Pracs/Prac11/DataStructures/HashTable/HashTable11.cpp
@@ -0,0 +1,50 @@
+﻿#include "HashTable11.h"
+
+#include <utility>
+
+HashTable11::HashTable11()
+{
+    table.resize(MAX_SIZE);
+}
+
+void HashTable11::insert(const std::string& key, int value)
+{
+    int index = hash(key);
+    for (auto& element : table[index])
+    {
+        if (element.first == key)
+        {
+            element.second = value;
+            return;
+        }
+    }
+    
+    table[index].emplace_back(key, value);
+}
+
+bool HashTable11::remove(const std::string& key)
+{
+    int index = hash(key);
+    for (auto iter = table[index].begin(); iter != table[index].end(); ++iter)
+    {
+        if (iter->first == key)
+        {
+            table[index].erase(iter);
+            return true;
+        }
+    }
+    return false;
+}
+
+std::optional<int> HashTable11::find(const std::string& key)
+{
+    int index = hash(key);
+    for (const auto& element : table[index])
+    {
+        if (element.first == key)
+        {
+            return element.second;
+        }
+    }
+    return std::nullopt;
+}
---
+++ Pracs/Prac11/DataStructures/HashTable/HashTable11.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <list>
+#include <optional>
+#include <string>
+#include <vector>
+
+class HashTable11
+{
+public:
+    HashTable11();
+
+    void insert(const std::string& key, int value);
+    bool remove(const std::string& key);
+    std::optional<int> find(const std::string& key);
+
+private:
+    int hash(const std::string& key) const
+    {
+        int total = 0;
+        for (char c : key)
+        {
+            total += c;
+        }
+        
+        return  total % MAX_SIZE;
+    }
+    
+private:
+    static constexpr  int MAX_SIZE = 10;
+    std::vector<std::list<std::pair<std::string, int>>> table;
+};
