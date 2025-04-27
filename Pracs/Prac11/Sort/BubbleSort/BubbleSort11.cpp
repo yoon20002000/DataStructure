bc0fc538959bd10d67dba3d669f382fa5bb0bf66
yoon20002000 <yoon20002000@naver.com>
2025-04-27 21:24:47 +0900

    BubbleSort11

---
+++ Pracs/Prac11/Sort/BubbleSort/BubbleSort11.cpp
@@ -0,0 +1,27 @@
+﻿#include "BubbleSort11.h"
+
+#include <memory>
+
+void BubbleSort11::sort(int* arr, size_t size)
+{
+    if (arr == nullptr || size < 2)
+    {
+        return;
+    }
+    for (size_t i = 0; i < size - 1; ++i)
+    {
+        bool isSwapped = false;
+        for (size_t j = 0; j < size - 1 - i; ++j )
+        {
+            if (arr[j]> arr[j+1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+                isSwapped = true;
+            }
+        }
+        if (!isSwapped)
+        {
+            break;
+        }
+    }
+}
