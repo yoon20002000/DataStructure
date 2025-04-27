90e18415db679de05df49d9f0cfa98e1fc747264
yoon20002000 <yoon20002000@naver.com>
2025-04-27 21:28:09 +0900

    InsertionSort11

---
+++ Pracs/Prac11/Sort/InsertSort/InsertionSort11.cpp
@@ -0,0 +1,21 @@
+﻿#include "InsertionSort11.h"
+
+void InsertionSort11::sort(int* arr, size_t size)
+{
+    if (arr == nullptr || size <2)
+    {
+        return;
+    }
+
+    for (size_t i = 1; i < size; ++i)
+    {
+        int key = arr[i];
+        size_t j = i;
+        while (j > 0 && arr[j-1] > key)
+        {
+            arr[j] = arr[j-1];
+            --j;
+        }
+        arr[j] = key;
+    }
+}
