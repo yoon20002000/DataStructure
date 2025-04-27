2e0a97616d8a477b574895518b5d6abf8475c5cc
yoon20002000 <yoon20002000@naver.com>
2025-04-27 22:04:26 +0900

    Quick11

---
+++ Pracs/Prac11/Sort/QuickSort/Quick11.cpp
@@ -0,0 +1,79 @@
+﻿#include "Quick11.h"
+
+#include <list>
+
+void Quick11::sort(int* arr, int size)
+{
+    quickSortL(arr, 0, size - 1);
+    // quickSortH(arr, 0, size - 1);
+}
+
+void Quick11::quickSortL(int* arr, int left, int right)
+{
+    if (left < right)
+    {
+        int pivot = partitionL(arr, left, right);
+        quickSortL(arr, left, pivot - 1);
+        quickSortL(arr, pivot + 1, right);
+    }
+}
+
+void Quick11::quickSortH(int* arr, int left, int right)
+{
+    if (left < right)
+    {
+        int pivot = partitionH(arr, left, right);
+        quickSortH(arr, left, pivot);
+        quickSortH(arr, pivot + 1, right);
+    }
+}
+
+int Quick11::partitionL(int* arr, int left, int right)
+{
+    int pivotIndex = left;
+    int pivotItem = arr[pivotIndex];
+
+    for (int i = left + 1; i <= right; ++i)
+    {
+        if (pivotItem > arr[i])
+        {
+            ++pivotIndex;
+            std::swap(arr[pivotIndex], arr[i]);
+        }
+    }
+    std::swap(arr[pivotIndex], arr[left]);
+
+    return pivotIndex;
+}
+
+int Quick11::partitionH(int* arr, int left, int right)
+{
+    while (true)
+    {
+        int pivotItem = arr[left + (right - left) / 2];
+        int l = left - 1;
+        int r = right + 1;
+
+        do
+        {
+            l++;
+        }
+        while (arr[l] < pivotItem);
+
+        do
+        {
+            r--;
+        }
+        while (arr[r] > pivotItem);
+
+
+        if (l < r)
+        {
+            std::swap(arr[l], arr[r]);
+        }
+        else
+        {
+            return r;
+        }
+    }
+}
---
+++ Pracs/Prac11/Sort/QuickSort/Quick11.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+class Quick11
+{
+public:
+    void sort(int* arr, int size);
+private:
+    void quickSortL(int* arr, int left, int right);
+    void quickSortH(int* arr, int left, int right);
+    int partitionL(int* arr, int left, int right);
+    int partitionH(int* arr, int left, int right);
+};
