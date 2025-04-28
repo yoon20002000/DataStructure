59926492585b63eede10a009749c2b1654579d70
yoon20002000 <yoon20002000@naver.com>
2025-04-28 12:01:23 +0900

    Insertion12

---
+++ Pracs/Prac12/Sort/InsertSort/Insertion12.cpp
@@ -0,0 +1,22 @@
+﻿#include "Insertion12.h"
+
+void Insertion12::sort(std::span<int>& arr)
+{
+	if (arr.size() < 2)
+	{
+		return;
+	}
+
+	for (size_t i = 1; i < arr.size(); ++i)
+	{
+		int key = arr[i];
+		size_t j = i;
+		while (j > 0 && arr[j-1] > key)
+		{
+			arr[j] = arr[j - 1];
+			--j;
+		}
+
+		arr[j] = key;
+	}
+}
---
+++ Pracs/Prac12/Sort/InsertSort/Insertion12.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <span>
+class Insertion12
+{
+public:
+	static void sort(std::span<int>& arr);
+};
+
