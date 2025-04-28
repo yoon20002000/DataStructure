1c5c1556593daf311de0d00728fa5893b088dcc2
yoon20002000 <yoon20002000@naver.com>
2025-04-28 11:30:32 +0900

    HashTable12

---
+++ Pracs/Prac12/DataStructures/HashTable/HashTable12.cpp
@@ -0,0 +1,52 @@
+﻿#include "HashTable12.h"
+
+HashTable12::HashTable12() : table(MAX_CAPACITY)
+{
+}
+
+void HashTable12::insert(const std::string& key, int value)
+{
+	int index = hash(key);
+	for (auto& el : table[index])
+	{
+		if (el.first == key)
+		{
+			el.second = value;
+			return;
+		}
+	}
+
+	table[index].emplace_back(key, value);
+}
+
+void HashTable12::deleteValue(const std::string& key)
+{
+	int index = hash(key);
+	// C++20 최신 기법
+	std::erase_if(table[index], [&](const auto& elem) {return elem.first == key; });
+	
+	//for (auto iter = table[index].begin(); iter != table[index].end(); ++iter)
+	//{
+	//	if (iter->first == key)
+	//	{
+	//
+	//		table[index].erase(iter);
+	//		return;
+	//	}
+	//}
+}
+
+std::optional<valueType> HashTable12::get(const std::string& key) const
+{
+	int index = hash(key);
+
+	for (auto& el : table[index])
+	{
+		if (el.first == key)
+		{
+			
+			return el;
+		}
+	}
+	return std::nullopt;
+}
---
+++ Pracs/Prac12/DataStructures/HashTable/HashTable12.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <vector>
+#include <list>
+#include <string>
+#include <optional>
+using valueType = std::pair<std::string, int>;
+
+class HashTable12
+{
+public:
+	HashTable12();
+	void insert(const std::string& key, int value);
+	void deleteValue(const std::string& key);
+	std::optional<valueType> get(const std::string& key) const;
+
+private:
+	int hash(const std::string& key) const
+	{
+		int total = 0;
+		for (char c : key)
+		{
+			total += c;
+		}
+		return total % MAX_CAPACITY;
+	}
+private:
+	static const int MAX_CAPACITY = 10;
+	std::vector<std::list<valueType>> table;
+};
+
