4e167291b6cec527c87acf8f24dfb22a1ce1ad13
yoon20002000 <yoon20002000@naver.com>
2025-04-28 12:13:58 +0900

    Selection12

---
+++ Pracs/Prac12/Sort/SelectionSort/Selection12.cpp
@@ -0,0 +1,25 @@
+﻿#include "Selection12.h"
+#include <memory>
+void Selection12::sort(std::span<int>& arr)
+{
+	if (arr.size() < 2)
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < arr.size() - 1; ++i)
+	{
+		int minIndex = i;
+		for (size_t j = i + 1; j < arr.size(); ++j)
+		{
+			if (arr[minIndex] > arr[j])
+			{
+				minIndex = j;
+			}
+		}
+		if (minIndex != i)
+		{
+			std::swap(arr[minIndex], arr[i]);
+		}
+	}
+}
---
+++ Pracs/Prac12/Sort/SelectionSort/Selection12.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <span>
+class Selection12
+{
+public:
+	static void sort(std::span<int>& arr);
+};
+
