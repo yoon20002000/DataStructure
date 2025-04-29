f337d1edb3f7941de2fead9dce7bf181efcb9f59
yoon20002000 <yoon20002000@naver.com>
2025-04-29 11:20:04 +0900

    HashTable13

---
+++ Pracs/Prac13/DataStructures/HashTable/HashTable13.cpp
@@ -0,0 +1,47 @@
+﻿#include "HashTable13.h"
+
+HashTable13::HashTable13() : table(MAX_SIZE)
+{
+    
+}
+
+void HashTable13::insert(const std::string& key, int value)
+{
+    int index = hash(key);
+    for (valueType& element : table[index])
+    {
+        if (element.first == key)
+        {
+            element.second = value;
+            return;
+        }
+    }
+    table[index].emplace_back(key, value);
+}
+
+bool HashTable13::remove(const std::string& key)
+{
+    int index = hash(key);
+    for (auto iter = table[index].begin(); iter != table[index].end(); ++iter)
+    {
+        if (iter->first == key)
+        {
+            table[index].erase(iter);
+            return true;
+        }
+    }
+    return false;
+}
+
+std::optional<valueType> HashTable13::find(const std::string& key) const
+{
+    int index = hash(key);
+    for (const valueType& element : table[index])
+    {
+        if (element.first == key)
+        {
+            return element;
+        }
+    }
+    return std::nullopt;
+}
---
+++ Pracs/Prac13/DataStructures/HashTable/HashTable13.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <list>
+#include <optional>
+#include <string>
+#include <vector>
+
+using valueType = std::pair<std::string,int>;
+
+class HashTable13
+{
+public:
+    HashTable13();
+    void insert(const std::string& key, int value);
+    bool remove(const std::string& key);
+    std::optional<valueType> find(const std::string& key) const;
+private:
+    int hash(const std::string& key) const
+    {
+        int total = 0;
+        int prime = 31;
+        for (char c : key)
+        {
+            total = (total * prime + c) % MAX_SIZE;
+        }
+        return total;
+    }
+    
+private:
+    static constexpr int MAX_SIZE = 10;
+    std::vector<std::list<valueType>> table;
+};
