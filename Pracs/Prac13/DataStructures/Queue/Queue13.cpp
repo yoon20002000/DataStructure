2960247a2076b2e9eabf43c9d87d5e9b1711e555
yoon20002000 <yoon20002000@naver.com>
2025-04-29 09:13:26 +0900

    Queue13

---
+++ Pracs/Prac13/DataStructures/Queue/Queue13.cpp
@@ -0,0 +1,110 @@
+﻿#include "Queue13.h"
+
+Queue13::Queue13(size_t inCapacity) : queue(nullptr), capacity(inCapacity + 1), front(0), back(0)
+{
+    queue = new int[capacity];
+}
+
+Queue13::~Queue13()
+{
+    delete[] queue;
+}
+
+Queue13::Queue13(const Queue13& other) : queue(nullptr), capacity(other.capacity), front(other.front), back(other.back)
+{
+    queue = new int[capacity];
+}
+
+Queue13& Queue13::operator=(const Queue13& other)
+{
+    if (&other != this)
+    {
+        delete[] queue;
+        queue = new int[other.capacity];
+        std::copy_n(other.queue, other.capacity, queue);
+        capacity = other.capacity;
+        front = other.front;
+        back = other.back;
+    }
+    return *this;
+}
+
+Queue13::Queue13(Queue13&& other) noexcept : queue(other.queue), capacity(other.capacity), front(other.front), back(other.back)
+{
+    other.queue = nullptr;
+    other.capacity = 0;
+    other.front = 0;
+    other.back = 0;
+}
+
+Queue13& Queue13::operator=(Queue13&& other) noexcept
+{
+    if (&other != this)
+    {
+        delete[] queue;
+
+        queue = other.queue;
+        capacity = other.capacity;
+        front = other.front;
+        back = other.back;
+
+        other.queue = nullptr;
+        other.capacity = 0;
+        other.front = 0;
+        other.back = 0;
+    }
+    return *this;
+}
+
+void Queue13::enqueue(int value)
+{
+    if (isFull())
+    {
+        size_t newCapacity = capacity * 1.5;
+        if (newCapacity == capacity)
+        {
+            ++newCapacity;
+        }
+        reserve(newCapacity);
+    }
+
+    back = (back + 1) % capacity;
+    queue[back] = value;
+}
+
+std::optional<int> Queue13::dequeue()
+{
+    if (isEmpty())
+    {
+        return std::nullopt;
+    }
+    int returnValue = queue[front];
+    front = (front+1) % capacity;
+
+    return returnValue;
+}
+
+std::optional<int> Queue13::peek() const
+{
+    if (isEmpty())
+    {
+        return std::nullopt;
+    }
+
+    return queue[front];
+}
+
+void Queue13::reserve(size_t inCapacity)
+{
+    if (capacity >= inCapacity + 1)
+    {
+        return;
+    }
+    int* newQueue = new int[inCapacity + 1];
+
+    std::copy_n(queue, capacity, newQueue);
+    delete[] queue;
+    queue = newQueue;
+    
+    capacity = inCapacity + 1;
+}
---
+++ Pracs/Prac13/DataStructures/Queue/Queue13.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <optional>
+
+class Queue13
+{
+public:
+    Queue13(size_t inCapacity = 1);
+    ~Queue13();
+    Queue13(const Queue13& other);
+    Queue13& operator=(const Queue13& other);
+    Queue13(Queue13&& other) noexcept;
+    Queue13& operator=(Queue13&& other) noexcept;
+
+    void enqueue(int value);
+    std::optional<int> dequeue();
+    std::optional<int> peek() const;
+    
+    bool isEmpty() const
+    {
+        return front == back;
+    }
+    bool isFull() const
+    {
+        return (back + 1) % capacity == front;
+    }
+
+private:
+    void reserve(size_t inCapacity);
+private:
+    int* queue;
+    size_t capacity;
+    int front;
+    int back;
+};
