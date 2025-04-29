e29b8ccffae4a68175d95498ebe19731ce33184a
yoon20002000 <yoon20002000@naver.com>
2025-04-29 09:23:20 +0900

    Heap13

---
+++ Pracs/Prac13/DataStructures/Heap/Heap13.cpp
@@ -0,0 +1,73 @@
+﻿#include "Heap13.h"
+
+Heap13::Heap13(Heap13&& other) noexcept : heap(std::move(other.heap))
+{
+    other.heap.clear();
+}
+
+Heap13& Heap13::operator=(Heap13&& other) noexcept
+{
+    if (&other != this)
+    {
+        heap = std::move(other.heap);
+    }
+    return *this;
+}
+
+void Heap13::insert(int value)
+{
+    heap.push_back(value);
+    heapifyUp(heap.size() - 1);
+}
+
+std::optional<int> Heap13::extractMax()
+{
+    if (isEmpty())
+    {
+        return std::nullopt;
+    }
+    int returnValue = heap[0];
+    heap[0] = heap.back();
+    heap.pop_back();
+    heapifyDown(0);
+
+    return returnValue;
+}
+
+void Heap13::heapifyUp(int index)
+{
+    while (index > 0 && heap[index] > heap[parent(index)])
+    {
+        std::swap(heap[index], heap[parent(index)]);
+        index = parent(index);
+    }
+}
+
+void Heap13::heapifyDown(int index)
+{
+    while (true)
+    {
+        int l = left(index);
+        int r = right(index);
+        int largest = index;
+
+        if (l < heap.size() && heap[l] > heap[largest])
+        {
+            largest = l;
+        }
+        if (r < heap.size() && heap[r] > heap[largest])
+        {
+            largest = r;
+        }
+
+        if (largest == index)
+        {
+            break;
+        }
+        else
+        {
+            std::swap(heap[index], heap[largest]);
+            index = largest;
+        }
+    }
+}
---
+++ Pracs/Prac13/DataStructures/Heap/Heap13.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <optional>
+#include <vector>
+
+class Heap13
+{
+public:
+    Heap13() = default;
+    ~Heap13() = default;
+    Heap13(const Heap13& other) = default;
+    Heap13& operator=(const Heap13& other) = default;
+    Heap13(Heap13&& other) noexcept;
+    Heap13& operator=(Heap13&& other) noexcept;
+
+    void insert(int value);
+    std::optional<int> extractMax();
+
+    bool isEmpty() const
+    {
+        return heap.empty();
+    }
+
+    int parent(int index) const
+    {
+        return (index - 1) / 2;
+    }
+
+    int left(int index) const
+    {
+        return 2 * index + 1;
+    }
+
+    int right(int index) const
+    {
+        return 2 * index + 2;
+    }
+
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+
+private:
+    std::vector<int> heap;
+};
