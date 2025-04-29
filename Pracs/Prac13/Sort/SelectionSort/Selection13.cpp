065c2872255e1ef6e3a8efc01c8df99d9aae5b5b
yoon20002000 <yoon20002000@naver.com>
2025-04-29 12:00:09 +0900

    Selection13

---
+++ Pracs/Prac13/Sort/SelectionSort/Selection13.cpp
@@ -0,0 +1,24 @@
+﻿#include "Selection13.h"
+
+void Selection13::sort(std::span<int> arr)
+{
+    if (arr.size() < 2)
+    {
+        return;
+    }
+    for (size_t i = 0 ; i < arr.size() - 1; ++i)
+    {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < arr.size(); ++j)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            std::swap(arr[minIndex], arr[i]);    
+        }
+    }
+}
---
+++ Pracs/Prac13/Sort/SelectionSort/Selection13.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <span>
+
+class Selection13
+{
+public:
+    static void sort(std::span<int> arr);
+};
