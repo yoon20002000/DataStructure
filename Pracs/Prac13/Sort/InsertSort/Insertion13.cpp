e425e6f0abc53e26470e3e1f8445cb9126ba9b69
yoon20002000 <yoon20002000@naver.com>
2025-04-29 11:55:23 +0900

    Insertion13

---
+++ Pracs/Prac13/Sort/InsertSort/Insertion13.cpp
@@ -0,0 +1,20 @@
+﻿#include "Insertion13.h"
+
+void Insertion13::sort(std::span<int> arr)
+{
+    if (arr.size() < 2)
+    {
+        return;
+    }
+    for (size_t i = 1; i < arr.size(); ++i)
+    {
+        int key = arr[i];
+        size_t j = i;
+        while (j > 0 && arr[j - 1] > key)
+        {
+            arr[j] = arr[j - 1];
+            --j;
+        }
+        arr[j] = key;
+    }
+}
---
+++ Pracs/Prac13/Sort/InsertSort/Insertion13.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <span>
+
+class Insertion13
+{
+public:
+    static void sort(std::span<int> arr);
+};
