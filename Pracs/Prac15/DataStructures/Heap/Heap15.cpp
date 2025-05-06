4c3c126f4156dd7c847fc92998cb3ece825c3dce
yoon20002000 <yoon20002000@naver.com>
2025-05-07 08:55:01 +0900

    heap15

---
+++ Pracs/Prac15/DataStructures/Heap/Heap15.cpp
@@ -0,0 +1,74 @@
+﻿#include "Heap15.h"
+
+Heap15::Heap15(Heap15&& other) noexcept
+{
+    heap = std::move(other.heap);
+}
+
+Heap15& Heap15::operator=(Heap15&& other) noexcept
+{
+    if (&other != this)
+    {
+        heap = std::move(other.heap);
+    }
+    return *this;
+}
+
+void Heap15::insert(int value)
+{
+    heap.push_back(value);
+    heapifyUp(heap.size() - 1);
+}
+
+std::optional<int> Heap15::extractMax()
+{
+    if (heap.empty())
+    {
+        return std::nullopt;
+    }
+    int returnValue = heap[0];
+    heap[0]=heap.back();
+    heap.pop_back();
+    heapifyDown(0);
+    
+    return returnValue;
+}
+
+void Heap15::heapifyUp(int index)
+{
+    while (index > 0 && heap[index]> heap[parent(index)])
+    {
+        std::swap(heap[index], heap[parent(index)]);
+        index = parent(index);
+    }
+}
+
+void Heap15::heapifyDown(int index)
+{
+    while (true)
+    {
+        int l = left(index);
+        int r = right(index);
+        int largest = index;
+
+        if (l < heap.size() && heap[l]>heap[largest])
+        {
+            largest = l;
+        }
+        
+        if (r < heap.size() && heap[r]>heap[largest])
+        {
+            largest = r;
+        }
+        
+        if (largest != index)
+        {
+            std::swap(heap[index], heap[largest]);
+            index = largest;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
---
+++ Pracs/Prac15/DataStructures/Heap/Heap15.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <optional>
+#include <vector>
+
+class Heap15
+{
+public:
+    Heap15() = default;
+    ~Heap15() = default;
+    Heap15(const Heap15& other) = default;
+    Heap15& operator=(const Heap15& other) = default;
+    Heap15(Heap15&& other) noexcept;
+    Heap15& operator=(Heap15&& other) noexcept;
+
+    void insert(int value);
+    std::optional<int> extractMax();
+
+private:
+    int parent(int index) const
+    {
+        return (index - 1) / 2;
+    }
+    int left(int index) const
+    {
+        return 2*index + 1;
+    }
+    int right(int index) const
+    {
+        return 2*index + 2;
+    }
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+private:
+    std::vector<int> heap;
+};
