0b1f905d0ee42acb7627f225115ed43e8c7486cd
yoon20002000 <yoon20002000@naver.com>
2025-05-07 09:31:50 +0900

    HashTable15

---
+++ Pracs/Prac15/DataStructures/HashTable/HashTable15.cpp
@@ -0,0 +1,75 @@
+﻿#include "HashTable15.h"
+
+HashTable15::HashTable15() : table(MAX_CAPACITY) 
+{
+    
+}
+
+HashTable15::HashTable15(const HashTable15& other) : table(other.table)
+{
+}
+
+HashTable15& HashTable15::operator=(const HashTable15& other)
+{
+    if (&other != this)
+    {
+        table = other.table;
+    }
+    return *this;
+}
+
+HashTable15::HashTable15(HashTable15&& other) noexcept : table(std::move(other.table))
+{
+}
+
+HashTable15& HashTable15::operator=(HashTable15&& other) noexcept
+{
+    if (&other != this)
+    {
+        table = std::move(other.table);
+    }
+    return *this;
+}
+
+void HashTable15::insert(const valuePair& pair)
+{
+    int index = hash(pair.first);
+
+    for (auto& element : table[index])
+    {
+        if (element.first == pair.first)
+        {
+            element.second = pair.second;
+            return;
+        }
+    }
+    table[index].push_back(pair);
+}
+
+std::optional<valuePair> HashTable15::search(const std::string& key) const
+{
+    int index = hash(key);
+
+    for (auto element : table[index])
+    {
+        if (element.first == key)
+        {
+            return element;
+        }
+    }
+    return std::nullopt;
+}
+
+void HashTable15::deleteVP(const std::string& key)
+{
+    int index = hash(key);
+
+    for (auto iter = table[index].begin(); iter != table[index].end(); ++iter)
+    {
+        if (iter->first == key)
+        {
+            table[index].erase(iter);
+            return;
+        }
+    }
+}
---
+++ Pracs/Prac15/DataStructures/HashTable/HashTable15.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <list>
+#include <optional>
+#include <string>
+#include <vector>
+
+using valuePair = std::pair<std::string,int>;
+
+class HashTable15
+{
+public:
+    HashTable15();
+    ~HashTable15() = default;
+    HashTable15(const HashTable15& other);
+    HashTable15& operator=(const HashTable15& other);
+    HashTable15(HashTable15&& other) noexcept;
+    HashTable15& operator=(HashTable15&& other) noexcept;
+    
+    void insert(const valuePair& pair);
+    std::optional<valuePair> search(const std::string& key) const;
+    void deleteVP(const std::string& key);
+    
+private:
+    int hash(std::string key) const
+    {
+        int total = 0;
+        int prime = 13;
+        for (size_t i = 0; i < key.length(); i++)
+        {
+            total = total * prime + key[i];
+        }
+        return abs(total) % MAX_CAPACITY;
+    }
+    
+private:
+    static constexpr int MAX_CAPACITY = 10;  
+    std::vector<std::list<valuePair>> table;
+};
