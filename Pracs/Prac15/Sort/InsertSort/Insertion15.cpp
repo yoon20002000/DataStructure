b813bf27e29aa9f4c4c3f2f03c3a0a83ad5b84ae
yoon20002000 <yoon20002000@naver.com>
2025-05-07 11:45:50 +0900

    Insertion 15

---
+++ Pracs/Prac15/Sort/InsertSort/Insertion15.cpp
@@ -0,0 +1,23 @@
+﻿#include "Insertion15.h"
+
+void Insertion15::sort(std::span<int>& arr)
+{
+    size_t size = arr.size();
+    if (size < 2)
+    {
+        return;
+    }
+
+    for (size_t i = 1; i < size; ++i)
+    {
+        int pivotItem = arr[i];
+        size_t j = i;
+        while (j > 0 && pivotItem < arr[j - 1])
+        {
+            arr[j]=arr[j-1];
+            --j;
+        }
+        
+        arr[j] = pivotItem;
+    }
+}
---
+++ Pracs/Prac15/Sort/InsertSort/Insertion15.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <span>
+
+class Insertion15
+{
+public:
+    static void sort(std::span<int>& arr); 
+};
